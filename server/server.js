a466f3560c5672a65b95cf89c59fdf2935d7f610
sahilpunia04 <sahilpreet0407@gmail.com>
2026-03-27 00:21:17 +0530

    fix express wildcard route

--- server/server.js
+++ server/server.js
@@ -4,10 +4,14 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+// Serve static files
 app.use(express.static(path.join(__dirname, '../client/build')));
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+// FIXED route
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
 });
 
-app.listen(PORT, () => console.log(`Server running on ${PORT}`));
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
